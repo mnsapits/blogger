ca7b3fbe0b0d87c86a156b89614c305bd04dbdf7
mnsapits <mnsapits@umich.edu>
2016-07-13 19:42:25 +0000

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+  
+  def index
+    @tags = Tag.all
+  end
+  
+  def destroy
+    @tag = Tag.find(params[:id]).delete
+    @tags = Tag.all
+    
+    redirect_to tags_path(@tags)
+  end
+
+end
